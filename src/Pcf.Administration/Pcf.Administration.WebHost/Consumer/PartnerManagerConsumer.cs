03931380c27418c5f7ba009fe7b87d7cc5a0562c
Evgeny2015 <105362989+Evgeny2015@users.noreply.github.com>
2025-06-22 22:12:57 +0300

    add RabbitMQ

---
+++ src/Pcf.Administration/Pcf.Administration.WebHost/Consumer/PartnerManagerConsumer.cs
@@ -0,0 +1,26 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Pcf.Administration.Core.Abstractions.Repositories;
+using Pcf.Administration.Core.Domain.Administration;
+using Pcf.Administration.WebHost.Service;
+using Pcf.ReceivingFromPartner.Integration.Dto;
+using System.Threading.Tasks;
+
+namespace Pcf.RabbitMQ.Consumer
+{
+    public class PartnerManagerConsumer : IConsumer<NotifyAdminAboutPartnerManagerDto>
+    {
+        public PartnerManagerConsumer(IRepository<Employee> _employeeRepository) 
+        { 
+
+        }
+        public Task Consume(ConsumeContext<NotifyAdminAboutPartnerManagerDto> context)
+        {
+            var message = context.Message;
+
+            //if (await EmployeeService.UpdateAppliedPromocodesAsync(_employeeRepository, id))
+
+            return context.RespondAsync(message);
+        }
+    }
+}
---
+++ src/Pcf.Administration/Pcf.Administration.WebHost/Service/EmployeeService.cs
@@ -0,0 +1,24 @@
+﻿using Pcf.Administration.Core.Abstractions.Repositories;
+using Pcf.Administration.Core.Domain.Administration;
+using System;
+using System.Threading.Tasks;
+
+namespace Pcf.Administration.WebHost.Service
+{
+    public static class EmployeeService
+    {
+        public static async Task<bool> UpdateAppliedPromocodesAsync(IRepository<Employee> _employeeRepository, Guid id)
+        {
+            var employee = await _employeeRepository.GetByIdAsync(id);
+
+            if (employee == null)
+                return false;
+
+            employee.AppliedPromocodesCount++;
+
+            await _employeeRepository.UpdateAsync(employee);
+
+            return true;
+        }
+    }
+}
